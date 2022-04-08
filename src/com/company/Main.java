cb79d7a76b8fa15fb93f26399266fd4954f686b8
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-04-08 21:24:51 +0600

    hello again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,23 @@
 package com.company;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+
 
 public class Main {
+
+
+    public static final GsonBuilder BUILDER = new GsonBuilder();
+    public static final Gson GSON = BUILDER.setPrettyPrinting().create();
+    public static final Path WRITE_PATH = Paths.get("./cars.json");
+
 
     public static void main(String[] args) {
 
@@ -10,10 +27,33 @@ public class Main {
                 Car.makeCar("Honda TruckWagon",12000,Type.TRUCK)
         };
 
+        String json = GSON.toJson(cars);
+        System.out.println(json);
     }
+
+    private static void write(String object) {
+        Path write = Paths.get(String.valueOf(WRITE_PATH));
+        try {
+            Files.writeString(write, object, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    private static String readFile() {
+        String json = "";
+        try {
+            FileReader reader = new FileReader(String.valueOf(WRITE_PATH));
+            int a;
+            while ((a = reader.read()) != -1) {
+                json += (char)a;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
 }
 
 
-/*
-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
- */
